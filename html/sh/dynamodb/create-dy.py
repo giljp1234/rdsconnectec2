7515d8c6abb27dfacaacc2773c859896629cffdd
giljp1234 <giljp1234@naver.com>
2023-06-02 16:39:10 +0900

    mm

--- html/sh/dynamodb/create-dy.py
+++ html/sh/dynamodb/create-dy.py
@@ -3,6 +3,8 @@ import boto3
 dynamodb = boto3.resource('dynamodb', region_name='ap-northeast-2')
 
 table = dynamodb.Table('test')
+
+
 number = int(input("번호: "))
 name = str(input("이름: "))
 addr = str(input("주소: "))
@@ -16,3 +18,12 @@ table.put_item(
 )
 
 print("데이터 등록 완료")
+
+# 숙 제 3 : select 여러번하려면 데이터가 많아야되는데 
+# 반복문안에서 다른 데이터들을 저장하는 코드 만들어보기  (for문)
+
+# 숙 제 4 : 
+# array를 이용해서 put item 한번해서 
+# 10개의 데이터를 한번에 저장해보는 거 ????????????????????????????????????
+# 배열 구조로 된 파일을 만들어서 파일자체를 저장하는 방법
+
--- html/sh/dynamodb/update-dy.py
+++ html/sh/dynamodb/update-dy.py
@@ -12,7 +12,7 @@ table.update_item(
         'number': number,
         'name': name
     },
-    UpdateExpression='SET addr = :val1',
+    UpdateExpression='SET addr = :val1, ',
     ExpressionAttributeValues={
         ':val1': addr
     }
@@ -20,3 +20,9 @@ table.update_item(
 
 
 print("데이터 수정 완료")
+
+
+
+
+#숙 제 5번 : select랑 합쳐서 원하는 데이터값을 수정할 수 있게 ????? 
+# +++++++ 기존에 있는 걸 불러와서 문자열을 추가해서 넣을 수 있게 만들어보기..?????
